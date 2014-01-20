5a3bce49eafbb4aefae45bca453cb8add7b472c5
Christian <lalanne@gmail.com>
2014-01-20 23:47:25 +0000

    open_file method

--- utst/ImportStrategyTests.cpp
+++ utst/ImportStrategyTests.cpp
@@ -10,12 +10,14 @@ class DummyStrategy : public ImportStrategy{
     public:
         DummyStrategy(string file_name){}
 
+        void open_file() {}
         void import_meta_data(){}
         void import_data(){}
 };
 
 TEST(ImportStrategyTests, dummy){
     unique_ptr<ImportStrategy> dummy_strategy(new DummyStrategy("file_name"));
+    dummy_strategy->open_file();
     dummy_strategy->import_meta_data();
     dummy_strategy->import_data();
 
