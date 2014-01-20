01326e7b4dc4bd449b57cc9d2b8d3f0a924998f5
Christian <lalanne@gmail.com>
2014-01-20 23:23:30 +0000

    adding tests for ImportStrategy

---
+++ utst/ImportStrategyTests.cpp
@@ -0,0 +1,13 @@
+//#include "IntegerField.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(ImportStrategyTests, dummy){
+    //IntegerField x(5);
+    auto dummy_strategy = make_unique<DummyStrategy>
+
+    EXPECT_EQ(1, 0);
+}
+
