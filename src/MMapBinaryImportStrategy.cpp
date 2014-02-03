8ef41ec7078e760108e5e14a8cbf8646a9e12b16
Christian <lalanne@gmail.com>
2014-02-03 15:27:22 +0000

    adding exceptions instead of C error system

--- src/MMapBinaryImportStrategy.cpp
+++ src/MMapBinaryImportStrategy.cpp
@@ -1,5 +1,6 @@
 
 #include "MMapBinaryImportStrategy.hpp"
+#include "NotFileFoundException.hpp"
 
 #include <iostream>
 #include <fcntl.h>
@@ -12,11 +13,11 @@ MMapBinaryImportStrategy::MMapBinaryImportStrategy(string file_name) : _file_nam
 
 /*it is necessary to create an error handling system through exceptions*/
 void MMapBinaryImportStrategy::open_file(){
-    cout<<"opening "<<_file_name<<"\n";
     struct stat sb;
     int fd = open(_file_name.c_str(), O_RDONLY);
     if(fd == -1){
-        perror ("open");
+        //leaking resource, no close call!!!!!
+        throw NotFileFoundException();
     }
 
     if(fstat(fd, &sb) == -1){
@@ -34,7 +35,25 @@ void MMapBinaryImportStrategy::open_file(){
 }
 
 void MMapBinaryImportStrategy::import_meta_data(){
-    cout<<"importing meta data from "<<_file_name<<"\n";
+    unsigned short n_columns;
+    memcpy(&n_columns, data, sizeof(n_columns));
+    data = data + sizeof(n_columns);
+    meta->set_columns(n_columns);
+
+    unsigned int n_rows;
+    memcpy(&n_rows, data, sizeof(n_rows));
+    data = data + sizeof(n_rows);
+    meta->set_rows(n_rows);
+
+    for(int i = 0; i< n_columns; ++i){
+        char type_column;
+        memcpy(&type_column, data, sizeof(type_column));
+        data = data + sizeof(type_column);
+        meta->add_column_type(type_column);
+
+        string column_name = StringUtilities::read_string_type(data);
+        meta->add_column_name(column_name);
+    }
 }
 
 void MMapBinaryImportStrategy::import_data(){
