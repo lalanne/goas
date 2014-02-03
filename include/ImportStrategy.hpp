0e71f1dbbc794bb50dd1ebd7e4f73fef4e58c61c
Christian <lalanne@gmail.com>
2014-02-03 17:35:20 +0000

    returning meta

--- include/ImportStrategy.hpp
+++ include/ImportStrategy.hpp
@@ -1,10 +1,12 @@
 #ifndef IMPORT_STRATEGY_9Q87HE9Q8HE9F8HWE
 #define IMPORT_STRATEGY_9Q87HE9Q8HE9F8HWE
 
+#include "Meta.hpp"
+
 class ImportStrategy{
     public:
         virtual void open_file() = 0;
-        virtual void import_meta_data() = 0;
+        virtual Meta import_meta_data() = 0;
         virtual void import_data() = 0;
 };
 
