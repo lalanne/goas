bb1b3871135123d622931c7446c8f22306dd28c9
Christian <lalanne@gmail.com>
2014-02-03 15:27:55 +0000

    testing not findinf a file to open

--- utst/ImportStrategyTests.cpp
+++ utst/ImportStrategyTests.cpp
@@ -1,5 +1,6 @@
 #include "ImportStrategy.hpp"
 #include "MMapBinaryImportStrategy.hpp"
+#include "NotFileFoundException.hpp"
 
 #include <gtest/gtest.h>
 
@@ -25,13 +26,20 @@ TEST(ImportStrategyTests, dummy_import){
     EXPECT_EQ(1, 0);
 }
 
-TEST(ImportStrategyTests, mmap_binary_import){
+TEST(ImportStrategyTests, no_file_found_by_mmap_binary_strategy_import){
     unique_ptr<ImportStrategy> import_strategy(new MMapBinaryImportStrategy("binary_file_name"));
+
+    EXPECT_THROW(import_strategy->open_file(), NotFileFoundException);
+}
+
+TEST(ImportStrategyTests, 1){
+    unique_ptr<ImportStrategy> import_strategy(new MMapBinaryImportStrategy("binary_file_10_rows.dat"));
     import_strategy->open_file();
     import_strategy->import_meta_data();
     import_strategy->import_data();
 
     EXPECT_EQ(1, 0);
 }
+
 
 
